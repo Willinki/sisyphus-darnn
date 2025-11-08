155510da5d7f8d1db875e39fffc5e0b96663951c
Willinki <d.badalotti@campus.unimib.it>
2025-11-08 18:41:15 +0100

    fix: syntax

--- scripts/launchers/train_fc_baseline.sh
+++ scripts/launchers/train_fc_baseline.sh
@@ -3,7 +3,7 @@
 uv run python3.11 scripts/python_scripts/training_darnn.py \
     experiment=cifar10_features_s_dynamical \
     'data.kwargs.x_transform=identity,sign' \
-    'model.kwags.dim_hidden=100,200,400,800,1600,3200,6400' \
+    'model.kwargs.dim_hidden=100,200,400,800,1600,3200,6400' \
     'model.kwargs.strength_forth=5.0,4.0,3.0' \
     'model.kwargs.strength_back=0.9,1.1,0.5' \
     'model.kwargs.threshold_in=1.2,1.0,0.8' \
