77fc6840adebec00d17b1039df01535b2a06f02e
Willinki <d.badalotti@campus.unimib.it>
2025-11-08 20:32:26 +0100

    maybe a little smaller

--- scripts/launchers/train_fc_baseline.sh
+++ scripts/launchers/train_fc_baseline.sh
@@ -6,8 +6,5 @@ uv run python3.11 scripts/python_scripts/training_darnn.py \
     'model.kwargs.dim_hidden=100,200,400,800,1600,3200,6400' \
     'model.kwargs.strength_forth=5.0,4.0,3.0' \
     'model.kwargs.strength_back=0.9,1.1,0.5' \
-    'model.kwargs.threshold_in=1.2,1.0,0.8' \
-    'model.kwargs.threshold_j=1.2,1.0,0.8' \
-    'model.kwargs.threshold_out=1.2,1.0,0.8' \
     'model.kwargs.j_d=0.3,0.5,0.7' \
     --multirun
