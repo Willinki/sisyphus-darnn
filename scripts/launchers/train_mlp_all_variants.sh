9b66c276d83c87587fa65fc4077849cd56ebdbd5
Willinki <d.badalotti@campus.unimib.it>
2025-11-08 02:43:23 +0100

    feat: lets go all the way

--- scripts/launchers/train_mlp_all_variants.sh
+++ scripts/launchers/train_mlp_all_variants.sh
@@ -2,7 +2,7 @@
 uv run python3.11 scripts/python_scripts/training_mlp.py \
     experiment=cifar10_features_s_tanh_mlp \
     cluster=gpu \
-    'model.kwargs.hidden_dim=100,200,400,800,1600' \
+    'model.kwargs.hidden_dim=100,200,400,800,1600,3200' \
     'model.kwargs.gain=1.0,5.0' \
     'data.kwargs.x_transform=identity,sign' \
     --multirun
@@ -10,7 +10,7 @@ uv run python3.11 scripts/python_scripts/training_mlp.py \
 uv run python3.11 scripts/python_scripts/training_mlp.py \
     experiment=cifar10_features_s_binary_mlp \
     cluster=gpu \
-    'model.kwargs.hidden_dim=100,200,400,800,1600' \
+    'model.kwargs.hidden_dim=100,200,400,800,1600,3200' \
     'model.kwargs.gain=1.0,5.0' \
     'data.kwargs.x_transform=identity,sign' \
     --multirun
@@ -18,7 +18,7 @@ uv run python3.11 scripts/python_scripts/training_mlp.py \
 uv run python3.11 scripts/python_scripts/training_mlp.py \
     experiment=cifar10_features_s_clipped_mlp \
     cluster=gpu \
-    'model.kwargs.hidden_dim=100,200,400,800,1600' \
+    'model.kwargs.hidden_dim=100,200,400,800,1600,3200' \
     'model.kwargs.gain=1.0,5.0' \
     'data.kwargs.x_transform=identity,sign' \
     --multirun
