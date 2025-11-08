809b84094f0b6e77c9b548a656efe9c96dee2d76
Willinki <d.badalotti@campus.unimib.it>
2025-11-08 16:52:56 +0100

    fix: new parameters for experiments

--- scripts/launchers/train_mlp_all_variants.sh
+++ scripts/launchers/train_mlp_all_variants.sh
@@ -2,26 +2,38 @@
 uv run python3.11 scripts/python_scripts/training_mlp.py \
     experiment=cifar10_features_s_tanh_mlp \
     cluster=gpu \
-    'model.kwargs.hidden_dim=100,200,400,800,1600,3200' \
+    model.kwargs.optim=sgd \
+    'model.kwargs.hidden_dim=100,200,400,800,1600,3200,6400' \
     'model.kwargs.gain=1.0,5.0' \
-    'model.kwargs.lr=0.0002,0.005' \
+    'model.kwargs.lr=0.001,0.0002,0.005' \
     'data.kwargs.x_transform=identity,sign' \
     --multirun
 
 uv run python3.11 scripts/python_scripts/training_mlp.py \
     experiment=cifar10_features_s_binary_mlp \
     cluster=gpu \
-    'model.kwargs.hidden_dim=100,200,400,800,1600,3200' \
+    model.kwargs.optim=sgd \
+    'model.kwargs.hidden_dim=100,200,400,800,1600,3200,6400' \
     'model.kwargs.gain=1.0,5.0' \
-    'model.kwargs.lr=0.0002,0.005' \
+    'model.kwargs.lr=0.001,0.0002,0.005' \
     'data.kwargs.x_transform=identity,sign' \
     --multirun
 
 uv run python3.11 scripts/python_scripts/training_mlp.py \
     experiment=cifar10_features_s_clipped_mlp \
     cluster=gpu \
-    'model.kwargs.hidden_dim=100,200,400,800,1600,3200' \
+    model.kwargs.optim=sgd \
+    'model.kwargs.hidden_dim=100,200,400,800,1600,3200,6400' \
     'model.kwargs.gain=1.0,5.0' \
+    'model.kwargs.lr=0.001,0.0002,0.005' \
+    'data.kwargs.x_transform=identity,sign' \
+    --multirun
+
+uv run python3.11 scripts/python_scripts/training_mlp.py \
+    experiment=cifar10_features_s_relu_mlp \
+    cluster=gpu \
+    model.kwargs.optim=sgd \
+    'model.kwargs.hidden_dim=100,200,400,800,1600,3200,6400' \
     'model.kwargs.lr=0.001,0.0002,0.005' \
     'data.kwargs.x_transform=identity,sign' \
     --multirun
