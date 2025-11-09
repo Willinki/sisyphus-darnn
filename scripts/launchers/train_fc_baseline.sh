f610ab886a27edc8f44d1f43d8dc127182ae307c
Willinki <d.badalotti@campus.unimib.it>
2025-11-09 21:52:53 +0100

    fix: new ranges. no more binary data

--- scripts/launchers/train_fc_baseline.sh
+++ scripts/launchers/train_fc_baseline.sh
@@ -2,8 +2,7 @@
 
 uv run python3.11 scripts/python_scripts/training_darnn.py \
     experiment=cifar10_features_s_dynamical \
-    'data.kwargs.x_transform=identity,sign' \
-    'model.kwargs.dim_hidden=100,200,400,800,1600,3200,6400' \
+    'model.kwargs.dim_hidden=128,256,512,1024,2048,4096' \
     'model.kwargs.strength_forth=5.0,4.0,3.0' \
     'model.kwargs.strength_back=0.9,1.1,0.5' \
     'model.kwargs.j_d=0.3,0.5,0.7' \
--- scripts/launchers/train_perceptron.sh
+++ scripts/launchers/train_perceptron.sh
@@ -3,7 +3,7 @@ uv run python3.11 scripts/python_scripts/training_mlp.py \
     experiment=cifar10_features_s_perceptron \
     cluster=gpu \
     model.kwargs.optim=sgd \
-    'model.kwargs.proj_dim=100,200,400,800,1600,3200,6400' \
+    'model.kwargs.proj_dim=128,256,512,1024,2048,4096' \
     'model.kwargs.margin=1.0,0.8,1.2' \
     'model.kwargs.lr=0.001,0.0002,0.005' \
     'data.kwargs.x_transform=identity,sign' \
