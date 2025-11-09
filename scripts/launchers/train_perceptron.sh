89f51f203ca7c85e73038f44e1e603c097734910
Willinki <d.badalotti@campus.unimib.it>
2025-11-09 22:30:47 +0100

    optim does not work for perceptron

--- scripts/launchers/train_perceptron.sh
+++ scripts/launchers/train_perceptron.sh
@@ -2,7 +2,6 @@
 uv run python3.11 scripts/python_scripts/training_mlp.py \
     experiment=cifar10_features_s_perceptron \
     cluster=gpu \
-    model.kwargs.optim=sgd \
     'model.kwargs.proj_dim=128,256,512,1024,2048,4096' \
     'model.kwargs.margin=1.0,0.8,1.2' \
     'model.kwargs.lr=0.001,0.0002,0.005' \
