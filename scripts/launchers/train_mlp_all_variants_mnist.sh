414cc645555f56365c3e4a40673a96905aa4e474
Willinki <d.badalotti@campus.unimib.it>
2025-11-10 20:06:09 +0100

    no mnist relu

--- scripts/launchers/train_mlp_all_variants_mnist.sh
+++ scripts/launchers/train_mlp_all_variants_mnist.sh
@@ -25,11 +25,3 @@ uv run python3.11 scripts/python_scripts/training_mlp.py \
     'model.kwargs.gain=1.0,5.0' \
     'model.kwargs.lr=0.001,0.0002,0.005' \
     --multirun
-
-uv run python3.11 scripts/python_scripts/training_mlp.py \
-    experiment=mnist_relu_mlp \
-    cluster=gpu \
-    model.kwargs.optim=sgd \
-    'model.kwargs.hidden_dim=128,256,512,1024,2048' \
-    'model.kwargs.lr=0.001,0.0002,0.005' \
-    --multirun
