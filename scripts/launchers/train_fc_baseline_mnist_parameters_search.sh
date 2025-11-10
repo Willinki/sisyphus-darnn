8b1f87673f3ecb999326287b50d8850cea894857
Willinki <d.badalotti@campus.unimib.it>
2025-11-10 20:08:30 +0100

    wrong keyword

--- scripts/launchers/train_fc_baseline_mnist_parameters_search.sh
+++ scripts/launchers/train_fc_baseline_mnist_parameters_search.sh
@@ -3,7 +3,7 @@
 uv run python3.11 scripts/python_scripts/training_darnn.py \
     experiment=mnist_dynamical \
     model.kwargs.dim_hidden=128 \
-    epoch=100 \
+    epochs=100 \
     model.kwargs.strength_forth=4.0 \
     model.kwargs.strength_back=0.9 \
     model.kwargs.j_d=0.5 \
@@ -16,7 +16,7 @@ uv run python3.11 scripts/python_scripts/training_darnn.py \
 uv run python3.11 scripts/python_scripts/training_darnn.py \
     experiment=mnist_dynamical \
     model.kwargs.dim_hidden=256 \
-    epoch=100 \
+    epochs=100 \
     model.kwargs.strength_forth=4.0 \
     model.kwargs.strength_back=0.9 \
     'model.kwargs.j_d=0.5,0.7' \
@@ -29,7 +29,7 @@ uv run python3.11 scripts/python_scripts/training_darnn.py \
 uv run python3.11 scripts/python_scripts/training_darnn.py \
     experiment=mnist_dynamical \
     model.kwargs.dim_hidden=512 \
-    epoch=100 \
+    epochs=100 \
     model.kwargs.strength_forth=5.0 \
     model.kwargs.strength_back=0.9 \
     model.kwargs.j_d=0.5 \
@@ -42,7 +42,7 @@ uv run python3.11 scripts/python_scripts/training_darnn.py \
 uv run python3.11 scripts/python_scripts/training_darnn.py \
     experiment=mnist_dynamical \
     model.kwargs.dim_hidden=1024 \
-    epoch=100 \
+    epochs=100 \
     model.kwargs.strength_forth=4.0 \
     model.kwargs.strength_back=0.9 \
     'model.kwargs.j_d=0.7,0.9' \
@@ -55,7 +55,7 @@ uv run python3.11 scripts/python_scripts/training_darnn.py \
 uv run python3.11 scripts/python_scripts/training_darnn.py \
     experiment=mnist_dynamical \
     model.kwargs.dim_hidden=2048 \
-    epoch=100 \
+    epochs=100 \
     model.kwargs.strength_forth=4.0 \
     model.kwargs.strength_back=1.1 \
     'model.kwargs.j_d=0.7,0.9' \
