9fb113ebc8eea72e96cfbe5c2a41b889abe29d46
Willinki <d.badalotti@campus.unimib.it>
2025-11-10 20:07:36 +0100

    more epochs for clipped mnist

--- scripts/launchers/train_mlp_clipped_mnist.sh
+++ scripts/launchers/train_mlp_clipped_mnist.sh
@@ -3,6 +3,7 @@
 uv run python3.11 scripts/python_scripts/training_mlp.py \
     experiment=mnist_clipped_mlp \
     cluster=gpu \
+    epochs=100 \
     model.kwargs.optim=sgd \
     'model.kwargs.hidden_dim=128,256,512,1024,2048' \
     'model.kwargs.gain=1.0,5.0' \
