6e1ecf4c2ac6ac304ffcf684d4e33460bff114eb
Willinki <d.badalotti@campus.unimib.it>
2025-11-15 09:06:35 +0100

    bit of cleaning

--- scripts/launchers/train_fc_baseline_mnist_wback_local.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-uv run python3.11 scripts/python_scripts/training_darnn_with_lr.py \
-    experiment=mnist_dynamical_wback \
-    model.kwargs.threshold_back=1.0 \
-    optimizer.special.learning_rate=0.0000
--- scripts/launchers/train_mlp_clipped_emnist.sh
+++ scripts/launchers/train_mlp_clipped_emnist.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 uv run python3.11 scripts/python_scripts/training_mlp.py \
-    experiment=mnist_clipped_mlp \
+    experiment=emnist_clipped_mlp \
     cluster=gpu \
     epochs=100 \
     model.kwargs.optim=sgd \
+    'model.kwargs.hidden_dim=128,256,512,1024' \
+    'model.kwargs.gain=1.0' \
+    'model.kwargs.lr=0.001,0.0002,0.0005,0.0001,0.00005' \
+    'model.kwargs.argmax_margin=1.0,0.8,1.2,0.5,0.2' \
+    --multirun
