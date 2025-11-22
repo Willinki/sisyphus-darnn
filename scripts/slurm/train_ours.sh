bb7e7e1b7f41bc3e0f3941936781fc508a9950a7
Mattia Scardecchia <mattia.scardecchia@nyu.edu>
2025-11-22 09:23:08 -0500

    good sparsity config

--- scripts/slurm/train_ours.sh
+++ scripts/slurm/train_ours.sh
@@ -20,17 +20,16 @@ source "$(conda info --base)/etc/profile.d/conda.sh"
 # Conda
 conda run -p "$CONDA_ENV" python ${PROJECT_ROOT}/scripts/python_scripts/main.py \
     --multirun \
-    model.kwargs.dim_hidden=500 \
+    model.kwargs.dim_hidden=1000 \
     epochs=20 \
-    'wandb.tags=[ours,linear,tuning]' \
+    'wandb.tags=[ours,GOOD]' \
     model.name=fc-baseline-sparse \
-    +model.kwargs.sparsity=0.96 \
+    +model.kwargs.sparsity=0.99 \
     optimizer.weight_decay_j=0.01 \
     optimizer.learning_rate_j=0.02 \
     model.kwargs.threshold_j=1.4 \
     model.kwargs.j_d=0.9 \
-    optimizer.learning_rate_win=0.05 \
-    model.kwargs.threshold_in=1.4 \
-    model.kwargs.strength_forth=4.0 \
-    trainer.fake_dynamics.k=0.5 \
-    torch_clf.lr=0.001,0.002,0.003,0.004,0.005,0.006,0.007 \
+    optimizer.learning_rate_win=0.03 \
+    model.kwargs.threshold_in=1.6 \
+    model.kwargs.strength_forth=5.0 \
+    trainer.fake_dynamics.k=0.5
