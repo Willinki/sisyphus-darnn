bbaf7ef40ec88bb83988718acbda4db9c454f783
Willinki <d.badalotti@campus.unimib.it>
2025-11-26 17:54:32 +0100

    p

--- scripts/launchers/train_fc_baseline_mnist_gsd.sh
+++ scripts/launchers/train_fc_baseline_mnist_gsd.sh
@@ -19,7 +19,7 @@ uv run python scripts/python_scripts/main.py \
     model.kwargs.threshold_j=1.5 \
     model.kwargs.threshold_out=3.8 \
     model.kwargs.strength_forth=4.7\
-    model.kwargs.strength_back=1.0 \
+    'model.kwargs.strength_back=1.0,1.5,2.0' \
     optimizer.weight_decay_j=0.0004 \
     optimizer.weight_decay_win=0.0 \
     optimizer.weight_decay_wout=0.02 \
