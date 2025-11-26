4dd70c77c914fee1805266808a717a05bce9f79f
Mattia Scardecchia <mattia.scardecchia@nyu.edu>
2025-11-26 10:38:55 -0500

    config: final emnist experiments

--- scripts/slurm/train_ours.sh
+++ scripts/slurm/train_ours.sh
@@ -19,7 +19,41 @@ source "$(conda info --base)/etc/profile.d/conda.sh"
 
 # Conda
 conda run -p "$CONDA_ENV" python ${PROJECT_ROOT}/scripts/python_scripts/main.py \
-    --multirun \
+    model.kwargs.dim_hidden=6000 \
+    'wandb.tags=[ours,emnist,scalingH]' \
+    model.kwargs.strength_back=1.25 \
+
+conda run -p "$CONDA_ENV" python ${PROJECT_ROOT}/scripts/python_scripts/main.py \
+    model.kwargs.dim_hidden=5000 \
+    'wandb.tags=[ours,emnist,scalingH]' \
+    model.kwargs.strength_back=1.75 \
+
+conda run -p "$CONDA_ENV" python ${PROJECT_ROOT}/scripts/python_scripts/main.py \
+    model.kwargs.dim_hidden=4000 \
+    'wandb.tags=[ours,emnist,scalingH]' \
+    model.kwargs.strength_back=2.0 \
+
+conda run -p "$CONDA_ENV" python ${PROJECT_ROOT}/scripts/python_scripts/main.py \
+    model.kwargs.dim_hidden=3000 \
+    'wandb.tags=[ours,emnist,scalingH]' \
+    model.kwargs.strength_back=1.75 \
+
+conda run -p "$CONDA_ENV" python ${PROJECT_ROOT}/scripts/python_scripts/main.py \
+    model.kwargs.dim_hidden=2000 \
+    'wandb.tags=[ours,emnist,scalingH]' \
+    model.kwargs.strength_back=2.5 \
+
+conda run -p "$CONDA_ENV" python ${PROJECT_ROOT}/scripts/python_scripts/main.py \
     model.kwargs.dim_hidden=1000 \
-    epochs=5 \
-    'wandb.tags=[ours,emnist]'
+    'wandb.tags=[ours,emnist,scalingH]' \
+    model.kwargs.strength_back=3.5 \
+
+conda run -p "$CONDA_ENV" python ${PROJECT_ROOT}/scripts/python_scripts/main.py \
+    model.kwargs.dim_hidden=500 \
+    'wandb.tags=[ours,emnist,scalingH]' \
+    model.kwargs.strength_back=4.25 \
+
+conda run -p "$CONDA_ENV" python ${PROJECT_ROOT}/scripts/python_scripts/main.py \
+    model.kwargs.dim_hidden=250 \
+    'wandb.tags=[ours,emnist,scalingH]' \
+    model.kwargs.strength_back=5.5 \
