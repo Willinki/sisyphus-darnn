29a495057b4ddfc4607e315588698568b80587f8
Willinki <d.badalotti@campus.unimib.it>
2025-11-27 11:40:08 +0100

    p

--- scripts/launchers/train_fc_baseline_mnist_gsd.sh
+++ scripts/launchers/train_fc_baseline_mnist_gsd.sh
@@ -4,8 +4,8 @@ uv run python scripts/python_scripts/main.py \
     --config-name=ours_sparse_mnist_tuning \
     --multirun \
     model.kwargs.dim_hidden=1000 \
-    epochs=5 \
-    torch_clf.epochs=5 \
+    epochs=20 \
+    torch_clf.epochs=20 \
     "wandb.tags=[ours,mnist,tuning]" \
     data.kwargs.x_transform='identity' \
     data.kwargs.linear_projection=null \
@@ -18,12 +18,12 @@ uv run python scripts/python_scripts/main.py \
     model.kwargs.threshold_in=1.7 \
     model.kwargs.threshold_j=1.7 \
     model.kwargs.threshold_out=3.8 \
-    model.kwargs.strength_forth=4.7\
+    'model.kwargs.strength_forth=4.4,4.7,5.0' \
     model.kwargs.strength_back=1.7 \
-    'optimizer.weight_decay_j=0.0004,0.001' \
+    'optimizer.weight_decay_j=0.0004,0.0001' \
     'optimizer.weight_decay_win=0.001,0.01' \
     optimizer.weight_decay_wout=0.02 \
-    'optimizer.learning_rate_j=0.05,0.1,0.02' \
+    optimizer.learning_rate_j=0.05,0.1,0.02 \
     'optimizer.learning_rate_win=0.15,0.25' \
     optimizer.learning_rate_wout=0.17 \
     trainer.fake_dynamics.k=0.5
