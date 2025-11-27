bfe3b9895939f951a3dce17618d46053b6573fc4
Willinki <d.badalotti@campus.unimib.it>
2025-11-27 11:40:43 +0100

    p

--- scripts/launchers/train_fc_baseline_mnist_gsd.sh
+++ scripts/launchers/train_fc_baseline_mnist_gsd.sh
@@ -23,7 +23,7 @@ uv run python scripts/python_scripts/main.py \
     'optimizer.weight_decay_j=0.0004,0.0001' \
     'optimizer.weight_decay_win=0.001,0.01' \
     optimizer.weight_decay_wout=0.02 \
-    optimizer.learning_rate_j=0.05,0.1,0.02 \
+    optimizer.learning_rate_j=0.05 \
     'optimizer.learning_rate_win=0.15,0.25' \
     optimizer.learning_rate_wout=0.17 \
     trainer.fake_dynamics.k=0.5
