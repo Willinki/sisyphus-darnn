c93ef50f17594a476958578ad1e18e5f609d5bb6
Willinki <d.badalotti@campus.unimib.it>
2025-12-16 17:55:42 +0100

    new hyperparameters

--- scripts/launchers/train_fc_baseline_mnist_gsd_final.sh
+++ scripts/launchers/train_fc_baseline_mnist_gsd_final.sh
@@ -3,7 +3,7 @@
 # final good configuration. last tests
 uv run python scripts/python_scripts/main.py \
     --config-name=ours_sparse_mnist_tuning \
-    model.kwargs.dim_hidden=1000 \
+    model.kwargs.dim_hidden=916 \
     epochs=20 \
     torch_clf.epochs=20 \
     "wandb.tags=[ours,mnist,scaling-h]" \
@@ -28,38 +28,38 @@ uv run python scripts/python_scripts/main.py \
     optimizer.learning_rate_wout=0.17 \
     trainer.fake_dynamics.k=0.5
 
-#uv run python scripts/python_scripts/main.py \
-#    --multirun \
-#    --config-name=ours_sparse_mnist_tuning \
-#    model.kwargs.dim_hidden=2000 \
-#    epochs=20 \
-#    torch_clf.epochs=20 \
-#    "wandb.tags=[ours,mnist,scaling-h]" \
-#    data.kwargs.x_transform='identity' \
-#    data.kwargs.linear_projection=null \
-#    data.kwargs.batch_size=16 \
-#    model.name=fc-baseline-sparse-fully \
-#    model.kwargs.dim_data=784 \
-#    model.kwargs.sparsity=0.99 \
-#    model.kwargs.sparsity_win=0.9 \
-#    model.kwargs.j_d=0.95 \
-#    model.kwargs.threshold_in=1.78 \
-#    model.kwargs.threshold_j=1.78 \
-#    model.kwargs.threshold_out=7 \
-#    model.kwargs.strength_forth=5.0 \
-#    'model.kwargs.strength_back=1.3,1.62,1.9' \
-#    optimizer.weight_decay_j=0.00006 \
-#    optimizer.weight_decay_win=0.01 \
-#    optimizer.weight_decay_wout=0.02 \
-#    optimizer.learning_rate_j=0.058 \
-#    optimizer.learning_rate_win=0.159 \
-#    optimizer.learning_rate_wout=0.17 \
-#    trainer.fake_dynamics.k=0.5
+uv run python scripts/python_scripts/main.py \
+    --multirun \
+    --config-name=ours_sparse_mnist_tuning \
+    model.kwargs.dim_hidden=1858 \
+    epochs=20 \
+    torch_clf.epochs=20 \
+    "wandb.tags=[ours,mnist,scaling-h]" \
+    data.kwargs.x_transform='identity' \
+    data.kwargs.linear_projection=null \
+    data.kwargs.batch_size=16 \
+    model.name=fc-baseline-sparse-fully \
+    model.kwargs.dim_data=784 \
+    model.kwargs.sparsity=0.99 \
+    model.kwargs.sparsity_win=0.9 \
+    model.kwargs.j_d=0.95 \
+    model.kwargs.threshold_in=1.78 \
+    model.kwargs.threshold_j=1.78 \
+    model.kwargs.threshold_out=7 \
+    model.kwargs.strength_forth=5.0 \
+    'model.kwargs.strength_back=1.3,1.62,1.9' \
+    optimizer.weight_decay_j=0.00006 \
+    optimizer.weight_decay_win=0.01 \
+    optimizer.weight_decay_wout=0.02 \
+    optimizer.learning_rate_j=0.058 \
+    optimizer.learning_rate_win=0.159 \
+    optimizer.learning_rate_wout=0.17 \
+    trainer.fake_dynamics.k=0.5
 
 uv run python scripts/python_scripts/main.py \
     --config-name=ours_sparse_mnist_tuning \
     --multirun \
-    model.kwargs.dim_hidden=3000 \
+    model.kwargs.dim_hidden=2815 \
     epochs=20 \
     torch_clf.epochs=20 \
     "wandb.tags=[ours,mnist,scaling-h]" \
@@ -87,7 +87,7 @@ uv run python scripts/python_scripts/main.py \
 uv run python scripts/python_scripts/main.py \
     --config-name=ours_sparse_mnist_tuning \
     --multirun \
-    model.kwargs.dim_hidden=4000 \
+    model.kwargs.dim_hidden=3783 \
     epochs=20 \
     torch_clf.epochs=20 \
     "wandb.tags=[ours,mnist,scaling-h]" \
@@ -115,7 +115,7 @@ uv run python scripts/python_scripts/main.py \
 uv run python scripts/python_scripts/main.py \
     --multirun \
     --config-name=ours_sparse_mnist_tuning \
-    model.kwargs.dim_hidden=5000 \
+    model.kwargs.dim_hidden=4758 \
     epochs=20 \
     torch_clf.epochs=20 \
     "wandb.tags=[ours,mnist,scaling-h]" \
@@ -143,7 +143,7 @@ uv run python scripts/python_scripts/main.py \
 uv run python scripts/python_scripts/main.py \
     --config-name=ours_sparse_mnist_tuning \
     --multirun \
-    model.kwargs.dim_hidden=6000 \
+    model.kwargs.dim_hidden=5737 \
     epochs=20 \
     torch_clf.epochs=20 \
     "wandb.tags=[ours,mnist,scaling-h]" \
@@ -160,62 +160,6 @@ uv run python scripts/python_scripts/main.py \
     model.kwargs.threshold_out=7 \
     model.kwargs.strength_forth=5.0 \
     'model.kwargs.strength_back=1.0,0.7' \
-    optimizer.weight_decay_j=0.00006 \
-    optimizer.weight_decay_win=0.01 \
-    optimizer.weight_decay_wout=0.02 \
-    optimizer.learning_rate_j=0.058 \
-    optimizer.learning_rate_win=0.159 \
-    optimizer.learning_rate_wout=0.17 \
-    trainer.fake_dynamics.k=0.5
-
-uv run python scripts/python_scripts/main.py \
-    --config-name=ours_sparse_mnist_tuning \
-    --multirun \
-    model.kwargs.dim_hidden=500 \
-    epochs=20 \
-    torch_clf.epochs=20 \
-    "wandb.tags=[ours,mnist,scaling-h]" \
-    data.kwargs.x_transform='identity' \
-    data.kwargs.linear_projection=null \
-    data.kwargs.batch_size=16 \
-    model.name=fc-baseline-sparse-fully \
-    model.kwargs.dim_data=784 \
-    model.kwargs.sparsity=0.99 \
-    model.kwargs.sparsity_win=0.9 \
-    model.kwargs.j_d=0.95 \
-    model.kwargs.threshold_in=1.78 \
-    model.kwargs.threshold_j=1.78 \
-    model.kwargs.threshold_out=7 \
-    model.kwargs.strength_forth=5.0 \
-    'model.kwargs.strength_back=2.1,2.4' \
-    optimizer.weight_decay_j=0.00006 \
-    optimizer.weight_decay_win=0.01 \
-    optimizer.weight_decay_wout=0.02 \
-    optimizer.learning_rate_j=0.058 \
-    optimizer.learning_rate_win=0.159 \
-    optimizer.learning_rate_wout=0.17 \
-    trainer.fake_dynamics.k=0.5
-
-uv run python scripts/python_scripts/main.py \
-    --config-name=ours_sparse_mnist_tuning \
-    --multirun \
-    model.kwargs.dim_hidden=250 \
-    epochs=20 \
-    torch_clf.epochs=20 \
-    "wandb.tags=[ours,mnist,scaling-h]" \
-    data.kwargs.x_transform='identity' \
-    data.kwargs.linear_projection=null \
-    data.kwargs.batch_size=16 \
-    model.name=fc-baseline-sparse-fully \
-    model.kwargs.dim_data=784 \
-    model.kwargs.sparsity=0.99 \
-    model.kwargs.sparsity_win=0.9 \
-    model.kwargs.j_d=0.95 \
-    model.kwargs.threshold_in=1.78 \
-    model.kwargs.threshold_j=1.78 \
-    model.kwargs.threshold_out=7 \
-    model.kwargs.strength_forth=5.0 \
-    'model.kwargs.strength_back=2.1,2.4' \
     optimizer.weight_decay_j=0.00006 \
     optimizer.weight_decay_win=0.01 \
     optimizer.weight_decay_wout=0.02 \
--- scripts/launchers/train_fc_baseline_tiny_imagenet.sh
+++ scripts/launchers/train_fc_baseline_tiny_imagenet.sh
@@ -12,68 +12,7 @@
 uv run python scripts/python_scripts/main.py \
     --multirun \
     --config-name=ours_sparse_tinyimagenet_tuning \
-    model.kwargs.dim_hidden=325 \
-    epochs=20 \
-    torch_clf.epochs=20 \
-    torch_clf.lr=0.001 \
-    "wandb.tags=[ours,tiny-imagenet,scaling-h]" \
-    data.kwargs.x_transform='identity' \
-    data.kwargs.linear_projection=null \
-    data.kwargs.batch_size=16 \
-    model.name=fc-baseline-sparse-fully \
-    model.kwargs.dim_data=512 \
-    model.kwargs.sparsity=0.99 \
-    model.kwargs.sparsity_win=0.9 \
-    model.kwargs.j_d=0.90 \
-    model.kwargs.threshold_in=1.6 \
-    model.kwargs.threshold_j=1.4 \
-    model.kwargs.threshold_out=7.0 \
-    model.kwargs.strength_forth=5.00 \
-    'model.kwargs.strength_back=5.5' \
-    optimizer.weight_decay_j=0.01 \
-    optimizer.weight_decay_win=0.0 \
-    optimizer.weight_decay_wout=0.02 \
-    optimizer.learning_rate_j=0.02 \
-    optimizer.learning_rate_win=0.3 \
-    optimizer.learning_rate_wout=0.02 \
-    trainer.fake_dynamics.k=0.5
-#
-
-# final good configuration. last tests
-uv run python scripts/python_scripts/main.py \
-    --multirun \
-    --config-name=ours_sparse_tinyimagenet_tuning \
-    model.kwargs.dim_hidden=650 \
-    epochs=20 \
-    torch_clf.epochs=20 \
-    torch_clf.lr=0.0007 \
-    "wandb.tags=[ours,tiny-imagenet,scaling-h]" \
-    data.kwargs.x_transform='identity' \
-    data.kwargs.linear_projection=null \
-    data.kwargs.batch_size=16 \
-    model.name=fc-baseline-sparse-fully \
-    model.kwargs.dim_data=512 \
-    model.kwargs.sparsity=0.99 \
-    model.kwargs.sparsity_win=0.9 \
-    model.kwargs.j_d=0.90 \
-    model.kwargs.threshold_in=1.6 \
-    model.kwargs.threshold_j=1.4 \
-    model.kwargs.threshold_out=7.0 \
-    model.kwargs.strength_forth=5.00 \
-    'model.kwargs.strength_back=4.25,3.5' \
-    optimizer.weight_decay_j=0.01 \
-    optimizer.weight_decay_win=0.0 \
-    optimizer.weight_decay_wout=0.02 \
-    optimizer.learning_rate_j=0.02 \
-    optimizer.learning_rate_win=0.3 \
-    optimizer.learning_rate_wout=0.02 \
-    trainer.fake_dynamics.k=0.5
-
-
-uv run python scripts/python_scripts/main.py \
-    --multirun \
-    --config-name=ours_sparse_tinyimagenet_tuning \
-    model.kwargs.dim_hidden=975 \
+    model.kwargs.dim_hidden=1155 \
     epochs=20 \
     torch_clf.epochs=20 \
     torch_clf.lr=0.0007 \
@@ -102,7 +41,7 @@ uv run python scripts/python_scripts/main.py \
 uv run python scripts/python_scripts/main.py \
     --multirun \
     --config-name=ours_sparse_tinyimagenet_tuning \
-    model.kwargs.dim_hidden=1400 \
+    model.kwargs.dim_hidden=1660 \
     epochs=20 \
     torch_clf.epochs=20 \
     torch_clf.lr=0.0005 \
@@ -131,7 +70,7 @@ uv run python scripts/python_scripts/main.py \
 uv run python scripts/python_scripts/main.py \
     --multirun \
     --config-name=ours_sparse_tinyimagenet_tuning \
-    model.kwargs.dim_hidden=1725 \
+    model.kwargs.dim_hidden=2216 \
     epochs=20 \
     torch_clf.epochs=20 \
     torch_clf.lr=0.0005 \
@@ -160,7 +99,65 @@ uv run python scripts/python_scripts/main.py \
 uv run python scripts/python_scripts/main.py \
     --multirun \
     --config-name=ours_sparse_tinyimagenet_tuning \
-    model.kwargs.dim_hidden=2050 \
+    model.kwargs.dim_hidden=2817 \
+    epochs=20 \
+    torch_clf.epochs=20 \
+    torch_clf.lr=0.0005 \
+    "wandb.tags=[ours,tiny-imagenet,scaling-h]" \
+    data.kwargs.x_transform='identity' \
+    data.kwargs.linear_projection=null \
+    data.kwargs.batch_size=16 \
+    model.name=fc-baseline-sparse-fully \
+    model.kwargs.dim_data=512 \
+    model.kwargs.sparsity=0.99 \
+    model.kwargs.sparsity_win=0.9 \
+    model.kwargs.j_d=0.90 \
+    model.kwargs.threshold_in=1.6 \
+    model.kwargs.threshold_j=1.4 \
+    model.kwargs.threshold_out=7.0 \
+    model.kwargs.strength_forth=5.00 \
+    'model.kwargs.strength_back=2.5,2.0' \
+    optimizer.weight_decay_j=0.01 \
+    optimizer.weight_decay_win=0.0 \
+    optimizer.weight_decay_wout=0.02 \
+    optimizer.learning_rate_j=0.02 \
+    optimizer.learning_rate_win=0.3 \
+    optimizer.learning_rate_wout=0.02 \
+    trainer.fake_dynamics.k=0.5
+
+uv run python scripts/python_scripts/main.py \
+    --multirun \
+    --config-name=ours_sparse_tinyimagenet_tuning \
+    model.kwargs.dim_hidden=3458 \
+    epochs=20 \
+    torch_clf.epochs=20 \
+    torch_clf.lr=0.0005 \
+    "wandb.tags=[ours,tiny-imagenet,scaling-h]" \
+    data.kwargs.x_transform='identity' \
+    data.kwargs.linear_projection=null \
+    data.kwargs.batch_size=16 \
+    model.name=fc-baseline-sparse-fully \
+    model.kwargs.dim_data=512 \
+    model.kwargs.sparsity=0.99 \
+    model.kwargs.sparsity_win=0.9 \
+    model.kwargs.j_d=0.90 \
+    model.kwargs.threshold_in=1.6 \
+    model.kwargs.threshold_j=1.4 \
+    model.kwargs.threshold_out=7.0 \
+    model.kwargs.strength_forth=5.00 \
+    'model.kwargs.strength_back=2.5,2.0' \
+    optimizer.weight_decay_j=0.01 \
+    optimizer.weight_decay_win=0.0 \
+    optimizer.weight_decay_wout=0.02 \
+    optimizer.learning_rate_j=0.02 \
+    optimizer.learning_rate_win=0.3 \
+    optimizer.learning_rate_wout=0.02 \
+    trainer.fake_dynamics.k=0.5
+
+uv run python scripts/python_scripts/main.py \
+    --multirun \
+    --config-name=ours_sparse_tinyimagenet_tuning \
+    model.kwargs.dim_hidden=4134 \
     epochs=20 \
     torch_clf.epochs=20 \
     torch_clf.lr=0.0005 \
