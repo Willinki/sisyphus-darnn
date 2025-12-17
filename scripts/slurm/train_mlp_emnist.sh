aa40d7bb88aabf4383d57b2ee55b1b3e35f694a1
Willinki <d.badalotti@campus.unimib.it>
2025-12-17 19:43:33 +0100

    emnist mlp

---
+++ scripts/slurm/train_mlp_emnist.sh
@@ -0,0 +1,113 @@
+#!/bin/bash -e
+#SBATCH --job-name=train-mlp
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=32G
+#SBATCH --output=train-mlp_%j.out
+#SBATCH --error=train-mlp_%j.err
+
+#PROJECT_ROOT="${SLURM_SUBMIT_DIR}"
+#mkdir -p "${PROJECT_ROOT}/logs"
+#source "${PROJECT_ROOT}/scripts/slurm/constants.sh"
+#
+#module load anaconda3/2024.02
+#source "$(conda info --base)/etc/profile.d/conda.sh"
+
+#conda run -p "$CONDA_ENV" python ${PROJECT_ROOT}/scripts/python_scripts/training_mlp.py \
+uv run python scripts/python_scripts/training_mlp.py \
+    --multirun \
+    'master_seed=33,44,55,66,77' \
+    data.kwargs.x_transform='sign' \
+    data.kwargs.linear_projection=null \
+    model.kwargs.input_dim=100 \
+    model.kwargs.hidden_dim=600 \
+    model.kwargs.lr=0.003 \
+    epochs=20 \
+    'wandb.tags=[baseline,scalingH,emnist,non-hetero]' \
+    model.kwargs.use_bias=false \
+    model.kwargs.clamp=true \
+    model.kwargs.loss_type=cross_entropy \
+    model.kwargs.num_hidden_layers=2 \
+    data.kwargs.batch_size=16
+
+uv run python scripts/python_scripts/training_mlp.py \
+    --multirun \
+    'master_seed=33,44,55,66,77' \
+    data.kwargs.x_transform='sign' \
+    data.kwargs.linear_projection=null \
+    model.kwargs.input_dim=100 \
+    model.kwargs.hidden_dim=100 \
+    model.kwargs.lr=0.003 \
+    epochs=20 \
+    'wandb.tags=[baseline,scalingH,emnist,non-hetero]' \
+    model.kwargs.use_bias=false \
+    model.kwargs.clamp=true \
+    model.kwargs.loss_type=cross_entropy \
+    model.kwargs.num_hidden_layers=2 \
+    data.kwargs.batch_size=16
+
+uv run python scripts/python_scripts/training_mlp.py \
+    --multirun \
+    'master_seed=33,44,55,66,77' \
+    data.kwargs.x_transform='sign' \
+    data.kwargs.linear_projection=null \
+    model.kwargs.input_dim=100 \
+    model.kwargs.hidden_dim=200 \
+    model.kwargs.lr=0.003 \
+    epochs=20 \
+    'wandb.tags=[baseline,scalingH,emnist,non-hetero]' \
+    model.kwargs.use_bias=false \
+    model.kwargs.clamp=true \
+    model.kwargs.loss_type=cross_entropy \
+    model.kwargs.num_hidden_layers=2 \
+    data.kwargs.batch_size=16
+
+uv run python scripts/python_scripts/training_mlp.py \
+    --multirun \
+    'master_seed=33,44,55,66,77' \
+    data.kwargs.x_transform='sign' \
+    data.kwargs.linear_projection=null \
+    model.kwargs.input_dim=100 \
+    model.kwargs.hidden_dim=300 \
+    model.kwargs.lr=0.003 \
+    epochs=20 \
+    'wandb.tags=[baseline,scalingH,emnist,non-hetero]' \
+    model.kwargs.use_bias=false \
+    model.kwargs.clamp=true \
+    model.kwargs.loss_type=cross_entropy \
+    model.kwargs.num_hidden_layers=2 \
+    data.kwargs.batch_size=16
+
+uv run python scripts/python_scripts/training_mlp.py \
+    --multirun \
+    'master_seed=33,44,55,66,77' \
+    data.kwargs.x_transform='sign' \
+    data.kwargs.linear_projection=null \
+    model.kwargs.input_dim=100 \
+    model.kwargs.hidden_dim=400 \
+    model.kwargs.lr=0.003 \
+    epochs=20 \
+    'wandb.tags=[baseline,scalingH,emnist,non-hetero]' \
+    model.kwargs.use_bias=false \
+    model.kwargs.clamp=true \
+    model.kwargs.loss_type=cross_entropy \
+    model.kwargs.num_hidden_layers=2 \
+    data.kwargs.batch_size=16
+
+uv run python scripts/python_scripts/training_mlp.py \
+    --multirun \
+    'master_seed=33,44,55,66,77' \
+    data.kwargs.x_transform='sign' \
+    data.kwargs.linear_projection=null \
+    model.kwargs.input_dim=100 \
+    model.kwargs.hidden_dim=500 \
+    model.kwargs.lr=0.003 \
+    epochs=20 \
+    'wandb.tags=[baseline,scalingH,emnist,non-hetero]' \
+    model.kwargs.use_bias=false \
+    model.kwargs.clamp=true \
+    model.kwargs.loss_type=cross_entropy \
+    model.kwargs.num_hidden_layers=2 \
+    data.kwargs.batch_size=16
