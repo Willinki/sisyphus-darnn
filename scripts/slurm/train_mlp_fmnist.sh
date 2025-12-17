e473d6ee5ef065f4a3f1c429153dc0c472548838
Willinki <d.badalotti@campus.unimib.it>
2025-12-17 17:52:57 +0100

    mlp experiments

--- scripts/slurm/train_mlp_fmnist.sh
+++ scripts/slurm/train_mlp_fmnist.sh
@@ -18,14 +18,106 @@
 #conda run -p "$CONDA_ENV" python ${PROJECT_ROOT}/scripts/python_scripts/training_mlp.py \
 uv run python3.11 scripts/python_scripts/training_mlp.py \
     --config-name=binary_mlp_fmnist \
+    --multirun \
+    'master_seed=33,44,55,66,77' \
+    data.name='fashion_mnist' \
+    data.kwargs.x_transform='identity' \
+    data.kwargs.linear_projection=null \
+    model.kwargs.input_dim=784 \
+    model.kwargs.hidden_dim=100 \
+    model.kwargs.lr=0.002 \
+    epochs=20 \
+    'wandb.tags=[baseline,scalingH,fmnist,non-hetero]' \
+    model.kwargs.use_bias=false \
+    model.kwargs.clamp=true \
+    model.kwargs.loss_type=cross_entropy \
+    model.kwargs.num_hidden_layers=2 \
+    data.kwargs.batch_size=16
+
+uv run python3.11 scripts/python_scripts/training_mlp.py \
+    --config-name=binary_mlp_fmnist \
+    --multirun \
+    'master_seed=33,44,55,66,77' \
+    data.name='fashion_mnist' \
+    data.kwargs.x_transform='identity' \
+    data.kwargs.linear_projection=null \
+    model.kwargs.input_dim=784 \
+    model.kwargs.hidden_dim=200 \
+    model.kwargs.lr=0.002 \
+    epochs=20 \
+    'wandb.tags=[baseline,scalingH,fmnist,non-hetero]' \
+    model.kwargs.use_bias=false \
+    model.kwargs.clamp=true \
+    model.kwargs.loss_type=cross_entropy \
+    model.kwargs.num_hidden_layers=2 \
+    data.kwargs.batch_size=16
+
+uv run python3.11 scripts/python_scripts/training_mlp.py \
+    --config-name=binary_mlp_fmnist \
+    --multirun \
+    'master_seed=33,44,55,66,77' \
+    data.name='fashion_mnist' \
+    data.kwargs.x_transform='identity' \
+    data.kwargs.linear_projection=null \
+    model.kwargs.input_dim=784 \
+    model.kwargs.hidden_dim=300 \
+    model.kwargs.lr=0.002 \
+    epochs=20 \
+    'wandb.tags=[baseline,scalingH,fmnist,non-hetero]' \
+    model.kwargs.use_bias=false \
+    model.kwargs.clamp=true \
+    model.kwargs.loss_type=cross_entropy \
+    model.kwargs.num_hidden_layers=2 \
+    data.kwargs.batch_size=16
+
+uv run python3.11 scripts/python_scripts/training_mlp.py \
+    --config-name=binary_mlp_fmnist \
+    --multirun \
+    'master_seed=33,44,55,66,77' \
+    data.name='fashion_mnist' \
+    data.kwargs.x_transform='identity' \
+    data.kwargs.linear_projection=null \
+    model.kwargs.input_dim=784 \
+    model.kwargs.hidden_dim=400 \
+    model.kwargs.lr=0.002 \
+    epochs=20 \
+    'wandb.tags=[baseline,scalingH,fmnist,non-hetero]' \
+    model.kwargs.use_bias=false \
+    model.kwargs.clamp=true \
+    model.kwargs.loss_type=cross_entropy \
+    model.kwargs.num_hidden_layers=2 \
+    data.kwargs.batch_size=16
+
+uv run python3.11 scripts/python_scripts/training_mlp.py \
+    --config-name=binary_mlp_fmnist \
+    --multirun \
+    'master_seed=33,44,55,66,77' \
+    data.name='fashion_mnist' \
+    data.kwargs.x_transform='identity' \
+    data.kwargs.linear_projection=null \
+    model.kwargs.input_dim=784 \
+    model.kwargs.hidden_dim=500 \
+    model.kwargs.lr=0.002 \
+    epochs=20 \
+    'wandb.tags=[baseline,scalingH,fmnist,non-hetero]' \
+    model.kwargs.use_bias=false \
+    model.kwargs.clamp=true \
+    model.kwargs.loss_type=cross_entropy \
+    model.kwargs.num_hidden_layers=2 \
+    data.kwargs.batch_size=16
+
+uv run python3.11 scripts/python_scripts/training_mlp.py \
+    --config-name=binary_mlp_fmnist \
+    --multirun \
+    'master_seed=33,44,55,66,77' \
     data.name='fashion_mnist' \
     data.kwargs.x_transform='identity' \
     data.kwargs.linear_projection=null \
     model.kwargs.input_dim=784 \
     model.kwargs.hidden_dim=600 \
-    model.kwargs.lr=0.009 \
+    model.kwargs.lr=0.002 \
     epochs=20 \
-    'wandb.tags=[baseline,scalingH,emnist,non-hetero]' \
+    'wandb.tags=[baseline,scalingH,fmnist,non-hetero]' \
     model.kwargs.use_bias=false \
     model.kwargs.clamp=true \
     model.kwargs.loss_type=cross_entropy \
--- scripts/slurm/train_mlp_tiny_imagenet.sh
+++ scripts/slurm/train_mlp_tiny_imagenet.sh
@@ -19,13 +19,115 @@
 uv run python3.11 scripts/python_scripts/training_mlp.py \
     --multirun \
     --config-name=binary_mlp_tinyimagenet \
+    'master_seed=33,44,55,66,77' \
     model.name="clipped-3layer-mlp" \
     data.name='tiny_imagenet' \
     data.kwargs.x_transform='identity' \
     data.kwargs.linear_projection=null \
     data.kwargs.label_mode='ooe' \
     model.kwargs.input_dim=512 \
-    model.kwargs.hidden_dim=300 \
+    'model.kwargs.hidden_dim=300,400' \
+    model.kwargs.lr=0.0001 \
+    epochs=20 \
+    'wandb.tags=[baseline,tiny_imagenet,non-hetero]' \
+    model.kwargs.use_bias=false \
+    model.kwargs.clamp=true \
+    model.kwargs.loss_type=cross_entropy \
+    model.kwargs.num_hidden_layers=2 \
+    data.kwargs.batch_size=16
+
+
+uv run python3.11 scripts/python_scripts/training_mlp.py \
+    --multirun \
+    --config-name=binary_mlp_tinyimagenet \
+    'master_seed=33,44,55,66,77' \
+    model.name="clipped-3layer-mlp" \
+    data.name='tiny_imagenet' \
+    data.kwargs.x_transform='identity' \
+    data.kwargs.linear_projection=null \
+    data.kwargs.label_mode='ooe' \
+    model.kwargs.input_dim=512 \
+    'model.kwargs.hidden_dim=500,600' \
+    model.kwargs.lr=0.0001 \
+    epochs=20 \
+    'wandb.tags=[baseline,tiny_imagenet,non-hetero]' \
+    model.kwargs.use_bias=false \
+    model.kwargs.clamp=true \
+    model.kwargs.loss_type=cross_entropy \
+    model.kwargs.num_hidden_layers=2 \
+    data.kwargs.batch_size=16
+
+uv run python3.11 scripts/python_scripts/training_mlp.py \
+    --multirun \
+    --config-name=binary_mlp_tinyimagenet \
+    'master_seed=33,44,55,66,77' \
+    model.name="clipped-3layer-mlp" \
+    data.name='tiny_imagenet' \
+    data.kwargs.x_transform='identity' \
+    data.kwargs.linear_projection=null \
+    data.kwargs.label_mode='ooe' \
+    model.kwargs.input_dim=512 \
+    model.kwargs.hidden_dim=700 \
+    model.kwargs.lr=0.0001 \
+    epochs=20 \
+    'wandb.tags=[baseline,tiny_imagenet,non-hetero]' \
+    model.kwargs.use_bias=false \
+    model.kwargs.clamp=true \
+    model.kwargs.loss_type=cross_entropy \
+    model.kwargs.num_hidden_layers=2 \
+    data.kwargs.batch_size=16
+
+uv run python3.11 scripts/python_scripts/training_mlp.py \
+    --multirun \
+    --config-name=binary_mlp_tinyimagenet \
+    'master_seed=33,44,55,66,77' \
+    model.name="clipped-3layer-mlp" \
+    data.name='tiny_imagenet' \
+    data.kwargs.x_transform='identity' \
+    data.kwargs.linear_projection=null \
+    data.kwargs.label_mode='ooe' \
+    model.kwargs.input_dim=512 \
+    model.kwargs.hidden_dim=800 \
+    model.kwargs.lr=0.0001 \
+    epochs=20 \
+    'wandb.tags=[baseline,tiny_imagenet,non-hetero]' \
+    model.kwargs.use_bias=false \
+    model.kwargs.clamp=true \
+    model.kwargs.loss_type=cross_entropy \
+    model.kwargs.num_hidden_layers=2 \
+    data.kwargs.batch_size=16
+
+uv run python3.11 scripts/python_scripts/training_mlp.py \
+    --multirun \
+    --config-name=binary_mlp_tinyimagenet \
+    'master_seed=33,44,55,66,77' \
+    model.name="clipped-3layer-mlp" \
+    data.name='tiny_imagenet' \
+    data.kwargs.x_transform='identity' \
+    data.kwargs.linear_projection=null \
+    data.kwargs.label_mode='ooe' \
+    model.kwargs.input_dim=512 \
+    model.kwargs.hidden_dim=900 \
+    model.kwargs.lr=0.0001 \
+    epochs=20 \
+    'wandb.tags=[baseline,tiny_imagenet,non-hetero]' \
+    model.kwargs.use_bias=false \
+    model.kwargs.clamp=true \
+    model.kwargs.loss_type=cross_entropy \
+    model.kwargs.num_hidden_layers=2 \
+    data.kwargs.batch_size=16
+
+uv run python3.11 scripts/python_scripts/training_mlp.py \
+    --multirun \
+    --config-name=binary_mlp_tinyimagenet \
+    'master_seed=33,44,55,66,77' \
+    model.name="clipped-3layer-mlp" \
+    data.name='tiny_imagenet' \
+    data.kwargs.x_transform='identity' \
+    data.kwargs.linear_projection=null \
+    data.kwargs.label_mode='ooe' \
+    model.kwargs.input_dim=512 \
+    model.kwargs.hidden_dim=1000 \
     model.kwargs.lr=0.0001 \
     epochs=20 \
     'wandb.tags=[baseline,tiny_imagenet,non-hetero]' \
