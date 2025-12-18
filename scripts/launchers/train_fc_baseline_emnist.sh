82352f1bb3a214529a67293d019d59d3b7c18fd4
Willinki <d.badalotti@campus.unimib.it>
2025-12-18 12:36:48 +0100

    emnsist experiments

--- scripts/launchers/train_fc_baseline_emnist.sh
+++ scripts/launchers/train_fc_baseline_emnist.sh
@@ -5,8 +5,8 @@
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=32G
-#SBATCH --output=train-mlp_%j.out
-#SBATCH --error=train-mlp_%j.err
+#SBATCH --output=train-emnist_%j.out
+#SBATCH --error=train-emnist_%j.err
 
 
 # Conda
@@ -14,6 +14,7 @@ uv run python scripts/python_scripts/main.py \
     --multirun \
     'master_seed=33,44,55,66,77' \
     model.kwargs.dim_hidden=5603 \
+    wnadb.experiment='emnist_scaling' \
     'wandb.tags=[ours,emnist,scalingH]' \
     model.kwargs.strength_back=1.25 \
 
@@ -21,6 +22,7 @@ uv run python scripts/python_scripts/main.py \
     --multirun \
     'master_seed=33,44,55,66,77' \
     model.kwargs.dim_hidden=4612 \
+    wnadb.experiment='emnist_scaling' \
     'wandb.tags=[ours,emnist,scalingH]' \
     model.kwargs.strength_back=1.75 \
 
@@ -28,6 +30,7 @@ uv run python scripts/python_scripts/main.py \
     --multirun \
     'master_seed=33,44,55,66,77' \
     model.kwargs.dim_hidden=3626 \
+    wnadb.experiment='emnist_scaling' \
     'wandb.tags=[ours,emnist,scalingH]' \
     model.kwargs.strength_back=2.0 \
 
@@ -35,6 +38,7 @@ uv run python scripts/python_scripts/main.py \
     --multirun \
     'master_seed=33,44,55,66,77' \
     model.kwargs.dim_hidden=2646 \
+    wnadb.experiment='emnist_scaling' \
     'wandb.tags=[ours,emnist,scalingH]' \
     model.kwargs.strength_back=1.75 \
 
@@ -42,6 +46,7 @@ uv run python scripts/python_scripts/main.py \
     --multirun \
     'master_seed=33,44,55,66,77' \
     model.kwargs.dim_hidden=1683 \
+    wnadb.experiment='emnist_scaling' \
     'wandb.tags=[ours,emnist,scalingH]' \
     model.kwargs.strength_back=2.5 \
 
@@ -49,5 +54,6 @@ uv run python scripts/python_scripts/main.py \
     --multirun \
     'master_seed=33,44,55,66,77' \
     model.kwargs.dim_hidden=760 \
+    wnadb.experiment='emnist_scaling' \
     'wandb.tags=[ours,emnist,scalingH]' \
     model.kwargs.strength_back=3.5 \
