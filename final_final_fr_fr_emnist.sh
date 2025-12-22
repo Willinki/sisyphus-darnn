a21b75167616790b403cfaef8ee98c0bd32bd791
Mattia Scardecchia <mattia.scardecchia@nyu.edu>
2025-12-22 11:25:33 -0500

    final final fr fr

---
+++ final_final_fr_fr_emnist.sh
@@ -0,0 +1,31 @@
+python scripts/python_scripts/main.py \
+    --multirun \
+    'master_seed=33,44,55,66,77' \
+    model.kwargs.dim_hidden=6000 \
+    wandb.project='emnist_scaling' \
+    'wandb.tags=[ours,emnist,scalingH]' \
+    model.kwargs.strength_back=1.25
+
+python scripts/python_scripts/main.py \
+    --multirun \
+    'master_seed=33,44,55,66,77' \
+    model.kwargs.dim_hidden=5000 \
+    wandb.project='emnist_scaling' \
+    'wandb.tags=[ours,emnist,scalingH]' \
+    model.kwargs.strength_back=1.75
+
+python scripts/python_scripts/main.py \
+    --multirun \
+    'master_seed=33,44,55,66,77' \
+    model.kwargs.dim_hidden=4000 \
+    wandb.project='emnist_scaling' \
+    'wandb.tags=[ours,emnist,scalingH]' \
+    model.kwargs.strength_back=2.0
+
+python scripts/python_scripts/main.py \
+    --multirun \
+    'master_seed=33,44,55,66,77' \
+    model.kwargs.dim_hidden=3000 \
+    wandb.project='emnist_scaling' \
+    'wandb.tags=[ours,emnist,scalingH]' \
+    model.kwargs.strength_back=1.75
